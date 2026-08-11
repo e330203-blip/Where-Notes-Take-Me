abf6e9145af9bd05ca4d18a845264967ed7d9809
Seniha Erva Minnetoğlu <e330203@gmail.com>
2026-08-11 13:05:42 +0300

    ilk düzen

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (menuButton && navLinks) {
+  menuButton.addEventListener('click', () => {
+    const open = navLinks.classList.toggle('open');
+    menuButton.setAttribute('aria-expanded', String(open));
+  });
+}
